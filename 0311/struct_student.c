1039a2659017e149441716e752ef0380ad6db754
NamSangKyu <nam2626@gmail.com>
2021-03-11 12:42:09 +0900

    학생정보 삭제 완료

--- 0311/struct_student.c
+++ 0311/struct_student.c
@@ -7,17 +7,17 @@ struct student{
 	double score;
 };
 struct student arr[10];
-int index = 0;
+int sIndex = 0;
 //데이터 추가
 void AddStudent(){
-	if(index ==10){
+	if(sIndex ==10){
 		printf("더이상 저장될 공간이 없습니다\n");
 		return;
 	}
-	printf("학번 입력 :");	scanf("%s",arr[index].sno);
-	printf("이름 입력 :");	scanf("%s",arr[index].name);
-	printf("전공 입력 :");	scanf("%s",arr[index].major);
-	printf("평점 입력 :");	scanf("%lf",&arr[index].score);
+	printf("학번 입력 :");	scanf("%s",arr[sIndex].sno);
+	printf("이름 입력 :");	scanf("%s",arr[sIndex].name);
+	printf("전공 입력 :");	scanf("%s",arr[sIndex].major);
+	printf("평점 입력 :");	scanf("%lf",&arr[sIndex].score);
 	index++;//입력이 끝난 후 다음데이터 입력 받을 인덱스로 이동
 }
 //데이터 삭제
@@ -28,14 +28,14 @@ void DeleteStudent(){
 	scanf("%s",sno);
 	//삭제할 데이터를 조회
 	int i;
-	for(i=0;i<index;i++){
+	for(i=0;i<sIndex;i++){
 		if(strcmp(arr[i].sno,sno)==0){
 			//해당 인덱스에서 다음인덱스 값을 복사받음
 			int j;
-			for(j=i;j<index-1;j++)
+			for(j=i;j<sIndex-1;j++)
 				arr[j] = arr[j+1];
 			//인덱스 감소 
-			index--;
+			sIndex--;
 			printf("입력한 데이터를 정상적으로  삭제했습니다.\n");
 			return;
 		}
